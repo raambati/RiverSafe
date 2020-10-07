ce267816bb13d20351444908ce5d9cdf16e99128
Charsha007 <49556175+raambati@users.noreply.github.com>
2020-10-07 13:40:11 +0100

    Update s3.tf

--- src/s3.tf
+++ src/s3.tf
@@ -11,8 +11,8 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIASEGA5G4XVKWC6EWW"
-  secret_key = "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"
+  access_key = " "
+  secret_key = " "
 }
 
 // S3 bucket
